e8ae7a23886ebd84d51f622f601256e399084517
m110 <m110@m110.pl>
2014-10-18 12:38:50 +0200

    Add new Pos class with unit tests.

--- drpython/utils.py
+++ drpython/utils.py
@@ -2,4 +2,31 @@
 
 from collections import namedtuple
 
-Pos = namedtuple('Pos', 'x y')
+class Pos(object):
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        if isinstance(other, self.__class__):
+            return Pos(self.x + other.x, self.y + other.y)
+        elif isinstance(other, (tuple, list)):
+            return Pos(self.x + other[0], self.y + other[1])
+        else:
+            raise TypeError('Unknown object in Pos __add__')
+
+    __radd__ = __add__
+    __iadd__ = __add__
+
+    def __eq__(self, other):
+        if isinstance(other, self.__class__):
+            return self.x == other.x and self.y == other.y
+        elif isinstance(other, (tuple, list)):
+            return self.x == other[0] and self.y == other[1]
+        else:
+            raise TypeError('Unknown object in Pos __eq__')
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
---
+++ test/UtilsTest.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python2
+
+import unittest
+from drpython.utils import Pos
+
+class PosTest(unittest.TestCase):
+
+    def test_pos_init(self):
+        pos = Pos(1, 2)
+        self.assertEqual(pos.x, 1)
+        self.assertEqual(pos.y, 2)
+
+        pos = Pos(x=42, y=100)
+        self.assertEqual(pos.x, 42)
+        self.assertEqual(pos.y, 100)
+
+    def test_pos_equal(self):
+        pos_a = Pos(100, 200)
+        pos_b = Pos(100, 200)
+
+        self.assertTrue(pos_a == pos_b)
+
+        with self.assertRaises(TypeError):
+            _ = pos_a == {}
+
+    def test_pos_not_equal(self):
+        pos_a = Pos(100, 200)
+        pos_b = Pos(1, 2)
+
+        self.assertFalse(pos_a == pos_b)
+
+    def test_pos_equal_tuple(self):
+        pos = Pos(100, 200)
+        pos_tuple = (100, 200)
+        pos_list = [100, 200]
+
+        self.assertTrue(pos == pos_tuple)
+        self.assertTrue(pos == pos_list)
+
+    def test_pos_not_equal_tuple(self):
+        pos = Pos(100, 200)
+        pos_tuple = (1, 2)
+        pos_list = [3, 4]
+
+        self.assertFalse(pos == pos_tuple)
+        self.assertFalse(pos == pos_list)
+
+    def test_pos_add(self):
+        pos_a = Pos(100, 200)
+        pos_b = Pos(5, 10)
+
+        pos = pos_a + pos_b
+        self.assertEqual(pos.x, 105)
+        self.assertEqual(pos.y, 210)
+
+        pos = pos_b + pos_a
+        self.assertEqual(pos.x, 105)
+        self.assertEqual(pos.y, 210)
+
+        pos = pos_a
+        pos += pos_b
+        self.assertEqual(pos.x, 105)
+        self.assertEqual(pos.y, 210)
+
+
+    def test_pos_tuple(self):
+        pos_a = Pos(100, 200)
+        pos_tuple = (5, 10)
+
+        pos = pos_a + pos_tuple
+        self.assertEqual(pos.x, 105)
+        self.assertEqual(pos.y, 210)
+
+        pos = pos_a
+        pos += pos_tuple
+        self.assertEqual(pos.x, 105)
+        self.assertEqual(pos.y, 210)
+
